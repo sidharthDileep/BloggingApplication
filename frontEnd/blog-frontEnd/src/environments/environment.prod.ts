5324f5c9f24e495378b6a3249fdbc967103df365
sidharthDileep <sidharthdileep96@gmail.com>
2023-10-17 15:49:50 +0530

    Front end to s3 deployment

--- frontEnd/blog-frontEnd/src/environments/environment.prod.ts
+++ frontEnd/blog-frontEnd/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  authUrl : "http://localhost:9090/blog-auth-api/api/v1/auth",
-  readUrl : "http://localhost:9090/blog-read-api/api/v1/posts",
-  writeUrl : "http://localhost:9090/blog-write-api/api/v1/posts"
+  authUrl : "http://34.239.248.67:8083/api/v1/auth",
+  readUrl : "http://3.84.168.71:8082/api/v1/posts",
+  writeUrl : "http://34.228.197.252:8081/api/v1/posts"
 };
